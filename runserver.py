0bcb1ceb75a8850f701d9356eca66b73343568c2
Bob Colner <bcolner@gmail.com>
2015-01-02 12:58:58 -0800

    Update runserver.py
    host flask app on local network for docker dev.

--- runserver.py
+++ runserver.py
@@ -9,4 +9,4 @@ init_app(app, db)
 
 # Start a development web server if executed from the command line
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
